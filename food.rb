8c60258092c9febbc091816548fa3d33dee5614e
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-02-20 16:52:33 -0500

    added more methods

--- food.rb
+++ food.rb
@@ -3,9 +3,26 @@ require 'pry'
 class Food
     attr_accesor :name, :price
     # macro for setter and getter
+    @@all = []
+
     def initialize(name, price)
         @name = name
         @price = price
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+        # class method
+    end
+
+    def self.sorted
+        @@all.sort_by { |food| food.price }
+    end
+
+    def remove
+        @@all = @@all.reject { |f| f == self }
     end
 
     # def name=(name)
