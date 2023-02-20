b810603c2b1126af0d24704cda4a8406e45ff8ba
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-02-20 15:40:24 -0500

    adding instance variables, setter, getter, and pry

--- food.rb
+++ food.rb
@@ -1,5 +1,22 @@
-class Food
+require 'pry'
 
+class Food
+    def initialize(name, price)
+        @name = name
+        @price = price
+    end
+
+    def name=(name)
+        @name = name
+        # setter
+    end
+
+    def name
+        @name 
+        # getter
+    end
 end
 
 item = Food.new
+
+binding.pry
