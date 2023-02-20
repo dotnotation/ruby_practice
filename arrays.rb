f2a6ac5b8c42fc1707f395da90f610985ad09402
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-02-20 14:31:39 -0500

    added methods and notes

--- arrays.rb
+++ arrays.rb
@@ -6,7 +6,22 @@ array_one = ["Hello", "World"]
 array_two = Array.new(3, "")
 # put size (which is required) and type ^
 
+# destructive methods
 array_one.push("!")
 array_one.append("appended")
 array_one << "shoveled"
+array_one.insert(0, "inserted")
+array_one.unshift("unshifted")
+array_one.pop
+array_one.shift
+array_one.slice(2, 4)
+# index you want to start at, and then the number of items
+# slice is non destructive
+
+# array methods
+array.length
+array.size
+array.count 
+
+
 binding.pry
