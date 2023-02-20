bad3cea76dd17591602de0cf1edbfe0d9a4ab1a1
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-02-20 13:23:03 -0500

    added methods

---
+++ coutdown-to-midnight.rb
@@ -0,0 +1,14 @@
+def countdown(seconds)
+    while seconds > 0
+        puts "#{seconds} SECOND(S)!"
+        seconds -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds)
+    while seconds > 0
+        sleep(1)
+        seconds -= 1
+    end
+end
