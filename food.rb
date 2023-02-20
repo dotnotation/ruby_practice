8fab55e39e45e64861295627f1fabfd830173d59
Dorthy Thielsen <dorthythielsen@gmail.com>
2023-02-20 15:46:35 -0500

    added attr_accessor

--- food.rb
+++ food.rb
@@ -1,20 +1,22 @@
 require 'pry'
 
 class Food
+    attr_accesor :name, :price
+    # macro for setter and getter
     def initialize(name, price)
         @name = name
         @price = price
     end
 
-    def name=(name)
-        @name = name
-        # setter
-    end
+    # def name=(name)
+    #     @name = name
+    #     # setter
+    # end
 
-    def name
-        @name 
-        # getter
-    end
+    # def name
+    #     @name 
+    #     # getter
+    # end
 end
 
 item = Food.new
